bd1888f45116663e11662463b051848d263c6891
Lucas Pérez Rosario <alu0101399928@ull.edu.es>
2022-10-26 15:38:45 +0000

    Cambios finales en los tests y buen funcionamiento

--- tests/tests.rb
+++ tests/tests.rb
@@ -1,19 +1,22 @@
-require 'test/unit'
 require 'lib/funciones'
+require 'test/unit'
 
+puts "Hola test"
 class TestVehiculos < Test::Unit::TestCase
 
-    def test_alquiler_por_dia (dias, vehiculo)
+    def test_alquiler_por_dia
         assert_equal(60, alquiler_por_dia(3, 1))
         assert_equal(105, alquiler_por_dia(3, 2))
+        assert_equal(nil, alquiler_por_dia(3,4))
     end
     
-    def test_alquiler_con_descuento (dias, vehiculo)
+    def test_alquiler_con_descuento
         assert_equal(51, precio_con_descuento(3, 1))
         assert_equal(84, precio_con_descuento(3, 2))
+        assert_equal(nil, precio_con_descuento(3,4))
     end
 
-    def test_total_kilometros (odometro_inicio, odometro_fin)
+    def test_total_kilometros
         assert_equal(150, total_kilometros(100, 250))
     end
 
