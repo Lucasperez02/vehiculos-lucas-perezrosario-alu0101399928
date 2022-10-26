41cd5a1ab835791df65b2acfd2c6c0350691dadb
Lucas Pérez Rosario <alu0101399928@ull.edu.es>
2022-10-26 15:02:18 +0000

    Test de la función 2

--- tests/tests.rb
+++ tests/tests.rb
@@ -6,8 +6,11 @@ class TestVehiculos < Test:Unit:TestCase
     def test_alquiler_por_dia (dias, vehiculo)
         assert_equal(60, alquiler_por_dia(3, 1))
         assert_equal(105, alquiler_por_dia(3, 2))
-    end    
+    end
     
-
+    def test_alquiler_con_descuento (dias, vehiculo)
+        assert_equal(51, alquiler_con_descuento(3, 1))
+        assert_equal(84, alquiler_con_descuento(3, 2))
+    end    
 
 end
