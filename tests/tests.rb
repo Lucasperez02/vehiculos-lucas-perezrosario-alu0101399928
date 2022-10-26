56400adb97bc133a45665d38faa271af5fc63327
Lucas Pérez Rosario <alu0101399928@ull.edu.es>
2022-10-26 15:21:13 +0000

    Primer arreglo tests

--- tests/tests.rb
+++ tests/tests.rb
@@ -1,7 +1,7 @@
 require 'test/unit'
 require 'lib/funciones.rb'
 
-class TestVehiculos < Test:Unit:TestCase
+class TestVehiculos < Test::Unit::TestCase
 
     def test_alquiler_por_dia (dias, vehiculo)
         assert_equal(60, alquiler_por_dia(3, 1))
