4bc60f1acc9373f69b9513ac2f5fb7491b3824ad
Lucas Pérez Rosario <alu0101399928@ull.edu.es>
2022-10-26 15:00:57 +0000

    Función 2

--- lib/funciones.rb
+++ lib/funciones.rb
@@ -7,3 +7,14 @@ def alquiler_por_dia (dias, vehiculo)
       nil
     end  
 end
+
+def precio_con_descuento(dias, vehiculo)
+    if vehiculo == 1
+      alquiler_descuento = alquiler_por_dia(dias, vehiculo) - (alquiler_por_dia(dias, vehiculo) * 0.15)
+    elsif vehiculo == 2
+      alquiler_descuento = alquiler_por_dia(dias, vehiculo) - (alquiler_por_dia(dias, vehiculo) * 0.20)
+    else
+      nil
+    end
+  
+end
