f09be041f775f5d325ece09fe426bd26b125ee9b
Lucas Pérez Rosario <alu0101399928@ull.edu.es>
2022-11-04 13:11:12 +0000

    Error Expectativa Probando el set_nuevo_propietario

--- lib/Vehiculo.rb
+++ lib/Vehiculo.rb
@@ -9,60 +9,62 @@ class Vehiculo
 	def initialize(id, fabricante, año, modelo, mpg, precio, propietarios, vehiculo_ocasion, tipo_galon)
 
 	  #Excepciones para el id
-	  if not type(id) == int or id < 0 
+	  if not id.is_a? Numeric or id < 0 
 		raise Exception.new("El identificador del vehiculo debe ser un int positivo")
 	  end
 	  #Excepciones para el fabricante
-	  if not type(fabricante) == string
+	  if not fabricante.class == String
 		raise Exception.new("El fabricamte del vehiculo debe ser una string")
 	  end
 	  #Excepciones para el año
 	  if año < 1900 or año > 2022
 		raise Exception.new("El año del vehiculo no está entre 1900 y 2022")
         end
-	  if not type(año) == int
+	  if not año.is_a? Numeric
 		raise Exception.new("El identificador del vehiculo debe ser un int")
 	  end
 	  #Excepciones para el modelo
-	  if not type(modelo) == string
+	  if not modelo.class == String
 		raise Exception.new("El modelo del vehiculo debe ser una string")
 	  end
 	  #Excepciones para mpg
-	  if not type(mpg) == double or mpg <= 0
+	  if not mpg.is_a? Numeric or mpg <= 0
 		raise Exception.new("Las mpg del vehiculo debe ser un int positivo y distinto de 0")
 	  end
 	  #Excepciones para el precio
-	  if not type(precio) == int or precio <= 0
+	  if not precio.is_a? Numeric or precio <= 0
 		raise Exception.new("El precio del vehiculo debe ser un int positivo y distinto de 0")
 	  end
 	  #Excepciones para los propietarios
 	  if vehiculo_ocasion == false
-		propetarios.each do |i|
-	  		if not tye(propietarios[i][0]) == int or propietarios[i][0] < 0
-				raise Exception.new("El número de los propietarios del vehiculo debe ser un int positivo")
-	  		end
-	  		if not tye(propietarios[i][1]) == string
-				raise Exception.new("El nombre del propietario debe ser un string")
-	  		end			
-		end
-	  else
-		if not propietario.empty
+	# 	propietarios.each do |i|
+	#   		if not propietarios[i][0].is_a? Integer or propietarios[i][0] < 0
+	# 			raise Exception.new("El número de los propietarios del vehiculo debe ser un int positivo")
+	#   		end
+	#   		if not propietarios[i][1].class == String
+	# 			raise Exception.new("El nombre del propietario debe ser un string")
+	#   		end			
+	# 	end
+	#   else
+		if not propietarios.empty?
 			raise Exception.new("Un vehículo de ocasión no debe tener propietarios")
 		end
 	  end
 	  #Excepciones para vehiculo_ocasion
-	  if not type(vehiculo_ocasion) == bool
+	  if not vehiculo_ocasion.in? [true,false]
 		raise Exception.new("El vehiculo_ocasion del vehiculo debe ser un booleano")
 	  end
 	  #Excepciones para tipo_galon
-	  if not type(tipo_galon) == string or not tipo_galon == "UK" and not tipo_galon == "USA"
+	  if not tipo_galon.class == String or not tipo_galon == "UK" and not tipo_galon == "USA"
 		raise Exception.new("El tipo_galon del vehiculo debe ser una string 'UK' o 'USA'")
 	  end
-	  # INICIALIZANDO VARIABLES DE INSTANCIA 
+	  #INICIALIZANDO VARIABLES DE INSTANCIA 
 	  @id, @año = id, año
 	  @fabricante, @modelo = fabricante, modelo
 	  @precio = precio
-	  @vehiculo_ocasion, @propietario = vehiculo_ocasion, propietario
+	  @vehiculo_ocasion, @propietarios = vehiculo_ocasion, propietarios
 	  @mpg, @tipo_galon = mpg, tipo_galon
 	end
+
+
 end
--- spec/vehiculo_spec.rb
+++ spec/vehiculo_spec.rb
@@ -1,13 +1,13 @@
 require 'Vehiculo'
 
 describe Vehiculo do
-    #before (:all) do
-     # @veh1 = Vehiculo.new(33245, "Volkswagen", 1988, "GTI MK2", 39.8, 2000, [[333333333, "Pedro"],[123321123, "Ana"]], false, "UK")
-      #@veh2 = Vehiculo.new(58723, "Volkswagen", 2022, "Polo Startline", 35.2, 50000, [[765456982, "Andrés"]], false, "UK")
-      #@veh3 = Vehiculo.new(90415, "Volkswagen", 2019, "T-Cross", 30.5, 24600, [], true, "USA")
-    #   @veh4 = Vehiculo.new(23145, "Volkswagen", 2017, "Polo VI", 40.0, 7000, [[333333333, "Pedro"],[123321123, "Ana"],[679876123, "Diana"]], false, "UK")
-    #   @veh5 = Vehiculo.new(17092, "Volkswagen", 2012, "GTI MK7", 32.8, 8000, [], true, "UK")
-    # end
+    before (:all) do
+    #   @veh1 = Vehiculo.new(33245, "Volkswagen", 1988, "GTI MK2", 39.8, 2000, [[333333333, "Pedro"],[123321123, "Ana"]], false, "UK")
+        @veh2 = Vehiculo.new(58723, "Volkswagen", 2022, "Polo Startline", 35.2, 50000, [[765456982, "Andrés"]], false, "UK")
+        @veh3 = Vehiculo.new(90415, "Volkswagen", 2019, "T-Cross", 30.5, 24600, [], true, "USA")
+        @veh4 = Vehiculo.new(23145, "Volkswagen", 2017, "Polo VI", 40.0, 7000, [[333333333, "Pedro"],[123321123, "Ana"],[679876123, "Diana"]], false, "UK")
+        @veh5 = Vehiculo.new(17092, "Volkswagen", 2012, "GTI MK7", 32.8, 8000, [], true, "UK")
+    end
 
     it "Instancia de vehiculo" do
         expect{Vehiculo.new(-12345, "Volkswagen", 2000, "GTI", 30.6, 2000, [], true, "UK").to raise_error("El identificador del vehiculo debe ser un int positivo")}
@@ -23,6 +23,14 @@ describe Vehiculo do
         expect{Vehiculo.new(12345, "Volkswagen", 2000, "GTI", 30.6, 2000, [], algo, "UK").to raise_error("El vehiculo_ocasion del vehiculo debe ser un booleano")}
         expect{Vehiculo.new(12345, "Volkswagen", 2000, "GTI", 30.6, 2000, [], true, "EU").to raise_error("El tipo_galon del vehiculo debe ser una string 'UK' o 'USA'")}
         expect{Vehiculo.new(12345, "Volkswagen", 2000, "GTI", 30.6, 2000, [], true, "UK").not_to eq(nil)}
-    end    
+    end 
+
+    
+  it "Probando el set_nuevo_propietario" do
+	expect{@veh4.set_nuevo_propietario(385206174, "Lucas").not_to eq(nil)}
+	expect{@veh3.set_nuevo_propietario(385206174, "Lucas").to raise_error("Un vehículo de ocasión no debe tener propietarios")}
+	expect{@veh2.set_nuevo_propietario(-385206174, "Lucas").to raise_error("El número de los propietarios del vehiculo debe ser un int positivo")}
+	#expect{@veh1.set_nuevo_propietario(385206174, Lucas).to raise_error("El nombre del propietario debe ser un string")}
+  end
 
 end
